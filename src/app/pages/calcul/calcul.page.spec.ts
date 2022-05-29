10f84db33d4b9a97a0f3527f0dcfc297a7a71057
Schoolcyril <keuwohermann@gmail.com>
2022-05-29 22:08:22 +0100

    test

--- src/app/pages/calcul/calcul.page.spec.ts
+++ src/app/pages/calcul/calcul.page.spec.ts
@@ -1,20 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
+import { CalculPage } from './calcul.page';
 
-import { HomePage } from './home.page';
-
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('CalculPage', () => {
+  let component: CalculPage;
+  let fixture: ComponentFixture<CalculPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ CalculPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(CalculPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
